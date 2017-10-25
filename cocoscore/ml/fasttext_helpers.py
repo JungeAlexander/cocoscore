d9ec6f79ac1fdef3a3137e7c5924a5e50ca2a6d5
Alexander Junge <alexander.junge@gmail.com>
2017-10-25 10:16:25 +0200

    Add quantize call to fastText call generator

--- cocoscore/ml/fasttext_helpers.py
+++ cocoscore/ml/fasttext_helpers.py
@@ -32,17 +32,17 @@ def get_hyperparameter_distributions():
     return param_dict
 
 
-def fasttext_wrapper(train_file_path, test_file_path, param_dict, fasttext_path, probability_file_prefix, thread=1):
+def get_fasttext_calls(train_file_path, test_file_path, param_dict, fasttext_path, probability_file_prefix, thread=1):
     """
     Generates fastText command-line calls for training a supervised model and applying it to a test dataset.
 
     :param train_file_path: path to the training dataset
     :param test_file_path: path to the test dataset
-    :param param_dict: dictionary mapping hyperparameters to their values
+    :param param_dict: dictionary mapping fasttext hyperparameters to their values
     :param fasttext_path: path to the fastText executable
     :param probability_file_prefix: str, prefix for the output file with class probabilities for the test dataset
     :param thread: int, the number of threads to use
-    :return pair of str - fastText calls for training and testing
+    :return triple of str - fastText calls for training, testing, quantizing, testing with the quantized model
     """
     param_dict['-thread'] = thread
     train_args = ''
@@ -54,5 +54,23 @@ def fasttext_wrapper(train_file_path, test_file_path, param_dict, fasttext_path,
     prob_file_path += '.txt.gz'
 
     train_call = fasttext_path + ' supervised -input ' + train_file_path + ' -output model ' + train_args
-    predict_call = fasttext_path + ' predict-prob model.bin ' + test_file_path + ' 2 | gzip > ' + prob_file_path
-    return train_call, predict_call
+    predict_call_prefix = test_file_path + ' 2 | gzip > ' + prob_file_path
+    predict_call = fasttext_path + ' predict-prob model.bin ' + predict_call_prefix
+    compress_call = fasttext_path + ' quantize -input model -output model'
+    compress_predict_call = fasttext_path + ' predict-prob model.ftz ' + predict_call_prefix
+    return train_call, predict_call, compress_call, compress_predict_call
+
+
+def run_fasttext(train_file_path, test_file_path, param_dict, fasttext_path, probability_file_prefix, thread=1,
+                 ):
+    """
+    Generates fastText command-line calls for training a supervised model and applying it to a test dataset.
+
+    :param train_file_path: path to the training dataset
+    :param test_file_path: path to the test dataset
+    :param param_dict: dictionary mapping fasttext hyperparameters to their values
+    :param fasttext_path: path to the fastText executable
+    :param probability_file_prefix: str, prefix for the output file with class probabilities for the test dataset
+    :param thread: int, the number of threads to use:
+    """
+    pass
