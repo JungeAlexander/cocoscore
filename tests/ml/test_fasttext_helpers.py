718859cdb027a5c8e899434d7df173bca98c9e00
Alexander Junge <alexander.junge@gmail.com>
2017-10-26 09:38:41 +0200

    Start with test cases for fasttext wrappers

---
+++ tests/ml/test_fasttext_helpers.py
@@ -0,0 +1,14 @@
+import pandas
+import unittest
+
+import cocoscore.ml.fasttext_helpers as fth
+
+
+class CVTest(unittest.TestCase):
+
+    def test_train_call_parameters(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
