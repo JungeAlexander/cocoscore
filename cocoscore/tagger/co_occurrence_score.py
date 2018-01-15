c5c0847e61fde7a3e54085c925fce214cc4f778f
Alexander Junge <alexander.junge@gmail.com>
2018-01-15 14:59:21 +0100

    Adjust variable names such that taxon, disease, gene do not appear anymore

--- cocoscore/tagger/co_occurrence_score.py
+++ cocoscore/tagger/co_occurrence_score.py
@@ -7,7 +7,7 @@ from ..tools.file_tools import get_file_handle
 __author__ = 'Alexander Junge (alexander.junge@gmail.com)'
 
 
-def get_gene_disease_pairs(type_name_set, first_type, second_type):
+def get_entity_pairs(type_name_set, first_type, second_type):
     first_type_names = set()
     second_type_names = set()
     for type_name in type_name_set:
@@ -17,7 +17,7 @@ def get_gene_disease_pairs(type_name_set, first_type, second_type):
         elif mytype == second_type:
             second_type_names.add(type_name)
         else:
-            raise ValueError("Encountered unknown taxonomy ID {:d}.".format(mytype))
+            raise ValueError("Encountered unknown type {:d}.".format(mytype))
     if first_type != second_type:
         return itertools.product(first_type_names, second_type_names)
     else:
@@ -37,8 +37,8 @@ def process_current_pmid_score_lines(current_pmid_lines, serial_to_type_entity, 
         type_entities.add(type_entity)
         type_entity_to_sentences[type_entity].add((int(paragraph), int(sentence)))
         type_entity_to_paragraphs[type_entity].add(int(paragraph))
-    for gene_disease_pair in get_gene_disease_pairs(type_entities, first_type, second_type):
-        first_type_entity, second_type_entity = gene_disease_pair
+    for entity_pair in get_entity_pairs(type_entities, first_type, second_type):
+        first_type_entity, second_type_entity = entity_pair
         assert first_type_entity[0] == first_type
         assert second_type_entity[0] == second_type
         common_sentences = type_entity_to_sentences[first_type_entity] & type_entity_to_sentences[second_type_entity]
@@ -49,28 +49,28 @@ def process_current_pmid_score_lines(current_pmid_lines, serial_to_type_entity, 
 
 
 def load_matches_file(matches_file_path, entities_file, first_type, second_type):
-    serial_to_taxid_name = get_serial_to_taxid_name_mapper(entities_file, taxids=(first_type, second_type))
+    serial_to_type_name = get_serial_to_taxid_name_mapper(entities_file, taxids=(first_type, second_type))
     matches_file = get_file_handle(matches_file_path, matches_file_path.endswith('.gz'))
     try:
         current_pmid_lines = []
         for line in matches_file:
-            # Fields are: pmid, paragraph, sentence, start_match, end_match, matched, taxon, serial
+            # Fields are: pmid, paragraph, sentence, start_match, end_match, matched, type, serial
             line_split = line.rstrip().split('\t')
             if len(current_pmid_lines) > 0 and line_split[0] != current_pmid_lines[0][0]:
-                yield process_current_pmid_score_lines(current_pmid_lines, serial_to_taxid_name, first_type,
+                yield process_current_pmid_score_lines(current_pmid_lines, serial_to_type_name, first_type,
                                                        second_type)
                 current_pmid_lines = [line_split]
             else:
                 current_pmid_lines.append(line_split)
         if len(current_pmid_lines) > 0:
-            yield process_current_pmid_score_lines(current_pmid_lines, serial_to_taxid_name, first_type, second_type)
+            yield process_current_pmid_score_lines(current_pmid_lines, serial_to_type_name, first_type, second_type)
     finally:
         matches_file.close()
 
 
 def load_score_file_iterator(score_dict):
-    for gene_disease, pmid_paragraph_sentence_dict in score_dict.items():
-        gene, disease = gene_disease
+    for entity_pair, pmid_paragraph_sentence_dict in score_dict.items():
+        entity_1, entity_2 = entity_pair
         pmid_to_paragraphs = collections.defaultdict(set)
         pmid_to_sentences = collections.defaultdict(set)
         for pmid_paragraph_sentence, _ in pmid_paragraph_sentence_dict.items():
@@ -78,15 +78,15 @@ def load_score_file_iterator(score_dict):
             pmid_to_sentences[pmid].add((paragraph, sentence))
             pmid_to_paragraphs[pmid].add(paragraph)
         for pmid in pmid_to_sentences:
-            yield pmid, gene, disease, pmid_to_sentences[pmid], pmid_to_paragraphs[pmid]
+            yield pmid, entity_1, entity_2, pmid_to_sentences[pmid], pmid_to_paragraphs[pmid]
 
 
-def get_max_sentence_score(scores, sentence_co_mentions, pmid, gene, disease):
+def get_max_sentence_score(scores, sentence_co_mentions, pmid, entity_1, entity_2):
     sentence_scores = [0.0]
     for paragraph, sentence in sentence_co_mentions:
         key = (pmid, paragraph, sentence)
-        if (gene, disease) in scores and key in scores[(gene, disease)]:
-            sentence_scores.append(scores[(gene, disease)][(pmid, paragraph, sentence)])
+        if (entity_1, entity_2) in scores and key in scores[(entity_1, entity_2)]:
+            sentence_scores.append(scores[(entity_1, entity_2)][(pmid, paragraph, sentence)])
         else:
             # TODO return 0.5 and print message to debug logging if no sentence scores found?
             pass
@@ -105,9 +105,9 @@ def get_weighted_counts(matches_file_path, sentence_scores, entities_file, first
         if i > 0 and i % 100000 == 0 and not silent:
             print('Document', i)
         for matches in document_matches:
-            pmid, gene, disease, sentence_co_mentions, paragraph_co_mentions = matches
+            pmid, entity_1, entity_2, sentence_co_mentions, paragraph_co_mentions = matches
             if isinstance(sentence_scores, dict) and not ignore_scores:
-                sentence_score = get_max_sentence_score(sentence_scores, sentence_co_mentions, pmid, gene, disease)
+                sentence_score = get_max_sentence_score(sentence_scores, sentence_co_mentions, pmid, entity_1, entity_2)
             else:
                 if len(sentence_co_mentions) > 0:
                     sentence_score = 1
@@ -123,9 +123,9 @@ def get_weighted_counts(matches_file_path, sentence_scores, entities_file, first
 
             pair_score_update = sentence_score * sentence_weight + paragraph_score * paragraph_weight +\
                 document_score * document_weight
-            pair_scores[(gene, disease)] += pair_score_update
-            pair_scores[gene] += pair_score_update
-            pair_scores[disease] += pair_score_update
+            pair_scores[(entity_1, entity_2)] += pair_score_update
+            pair_scores[entity_1] += pair_score_update
+            pair_scores[entity_2] += pair_score_update
             pair_scores[None] += pair_score_update
     return dict(pair_scores)
 
@@ -154,7 +154,7 @@ def co_occurrence_score(matches_file_path, score_file_path, entities_file, first
     :param matches_file_path: matches file as produced by tagger. Used to define co-occurring terms.
     If this is None, co-occurrences are extracted from score_file_path.
     :param score_file_path: sentence score file (tsv formatted) with five columns: pmid, paragraph number, sentence
-    number, gene identifier, disease identifier, sentence score
+    number, first entity, second entity, sentence score
     :param entities_file: entities file as used by tagger
     :param first_type: int, type of the first entity class to be scored
     :param second_type: int, type of the second entity class to be scored
@@ -164,7 +164,7 @@ def co_occurrence_score(matches_file_path, score_file_path, entities_file, first
     :param weighting_exponent: exponent weight in the co-occurrence score
     :param ignore_scores: If True, sentence scores are ignored.
     :param silent: If True, no progress updates are printed
-    :return: a dictionary mapping gene, disease pairs to their co-occurrence scores
+    :return: a dictionary mapping entity pairs to their co-occurrence scores
     """
     if score_file_path is None:
         scores = None
@@ -180,9 +180,9 @@ def co_occurrence_score(matches_file_path, score_file_path, entities_file, first
     for key, score in weighted_counts.items():
         if not isinstance(key, tuple):
             continue
-        gene, disease = key
+        entity_1, entity_2 = key
         co_occurrence = (score ** weighting_exponent) * \
-                        (((score * norm_factor)/(weighted_counts[gene] * weighted_counts[disease])) **
+                        (((score * norm_factor)/(weighted_counts[entity_1] * weighted_counts[entity_2])) **
                          (1 - weighting_exponent))
         co_occurrence_scores[key] = co_occurrence
     return co_occurrence_scores
