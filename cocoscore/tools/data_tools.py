494e6990fb1b3034fd016729ceb7b40b4c95c204
Alexander Junge <alexander.junge@gmail.com>
2018-01-25 08:14:12 +0100

    Replace assertion for no missing values with raising a ValueError
    Raising a ValueError is more informative since it tells the user which
    dataset caused trouble.

--- cocoscore/tools/data_tools.py
+++ cocoscore/tools/data_tools.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 def load_data_frame(data_frame_path, sort_reindex=False, class_labels=True, match_distance=False):
     """
-    Load a sentence data set as pandas DataFrame from a given path.
+    Load a dataset as pandas DataFrame from a given path.
 
     :param data_frame_path: the path to load the pandas DataFrame from
     :param sort_reindex: if True, the returned data frame will be sorted by PMID and reindex by 0, 1, 2, ...
     :param class_labels: if True, the class label is assumed to be present as the second-to-last column
     :param match_distance: if True, the distance between the closest match is assumed to be present as the last column
     :return: a pandas DataFrame loaded from the given path
+    :raises ValueError if
     """
     column_names = ['pmid', 'paragraph', 'sentence', 'entity1', 'entity2', 'text']
     if class_labels:
@@ -21,5 +22,6 @@ def load_data_frame(data_frame_path, sort_reindex=False, class_labels=True, matc
     if sort_reindex:
         data_df.sort_values('pmid', axis=0, inplace=True, kind='mergesort')
         data_df.reset_index(inplace=True, drop=True)
-    assert data_df.isnull().sum().sum() == 0
+    if data_df.isnull().sum().sum() != 0:
+        raise ValueError(f'Encountered missing values while loading data from {data_frame_path}.')
     return data_df
