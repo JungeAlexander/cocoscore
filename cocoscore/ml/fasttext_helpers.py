2efabdd960318663b483bd8ace228040daa78cb5
Alexander Junge <alexander.junge@gmail.com>
2018-06-22 15:29:17 +0200

    Add optional word embeddings parameter to fasttext_fit_predict_default()

--- cocoscore/ml/fasttext_helpers.py
+++ cocoscore/ml/fasttext_helpers.py
@@ -121,7 +121,7 @@ def fasttext_predict(trained_model_path, test_file_path, fasttext_path, probabil
 
 
 def fasttext_fit_predict_default(train_df, test_df, dim=300, epochs=50, lr=0.005, wordngrams=2, ws=5,
-                                 bucket=2000000):
+                                 bucket=2000000, pretrained_vectors_path=None):
     """
     Fit and predict fastText with default parameters for given training and test set.
 
@@ -133,13 +133,13 @@ def fasttext_fit_predict_default(train_df, test_df, dim=300, epochs=50, lr=0.005
     :param wordngrams: fasttext parameter
     :param ws: fasttext parameter
     :param bucket: fasttext parameter
+    :param pretrained_vectors_path: pretrained word embeddings
     :return: tuple of training and test performance
     """
     fasttext_path = 'fasttext'
     thread = 1
     compress_model = True
     param_dict = {'-dim': dim, '-epoch': epochs, '-lr': lr, '-wordNgrams': wordngrams, '-ws': ws, '-bucket': bucket}
-    pretrained_vectors_path = None
     return _fasttext_fit_predict(train_df['text'].str.lower(),
                                  get_fasttext_classes(train_df),
                                  test_df['text'].str.lower(),
